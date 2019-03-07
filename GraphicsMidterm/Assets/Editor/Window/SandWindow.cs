40c12f658de1aeed0fb13b31aece85f4092db647
RJ Bourelais <361rjb@gmail,.com>
2019-03-07 12:13:49 -0500

    Created Unity Project
    Added unity project and started pipeline window editor.

---
+++ GraphicsMidterm/Assets/Editor/Window/SandWindow.cs
@@ -0,0 +1,30 @@
+﻿#if UNITY_EDITOR 
+
+using UnityEditor;
+using UnityEngine;
+
+public class SandWindow : EditorWindow
+{
+    [MenuItem ("Tool/Sand Shader/Pipeline Window")]
+    static void Init()
+    {
+        EditorWindow.GetWindow(typeof(SandWindow));
+    }
+
+    void OnGUI()
+    {
+        Rect menuRect = new Rect(0, 0, 200, 180);
+        BeginWindows();
+        menuRect = GUILayout.Window(1, menuRect, MenuWindow, "Menu Window");
+        EndWindows();
+        
+    }
+
+
+    void MenuWindow(int unusedWindowID)
+    {
+        GUILayout.Button("Name");
+    }
+}
+
+#endif
