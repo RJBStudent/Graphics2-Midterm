e01f162637c096e2cea8cddf77bc0bc0b7022e58
361rjb <361rjb@gmail.com>
2019-03-21 22:32:17 -0400

    Added files for real this time

---
+++ GraphicsMidterm/Assets/Scripts/TestScripts.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Runtime.InteropServices;
+
+public class TestScripts : MonoBehaviour
+{
+#if UNITY_IPHONE
+   
+       // On iOS plugins are statically linked into
+       // the executable, so we have to use __Internal as the
+       // library name.
+       [DllImport ("__Internal")]
+
+#else
+
+    // Other platforms load plugins dynamically, so pass the name
+    // of the plugin's dynamic library.
+    [DllImport("animal3D-DemoProject")]
+    static extern int TestOutput();
+
+#endif
+
+    //private static extern int TestOutput();
+
+    void Start()
+    {
+        Debug.Log(TestOutput());
+    }
+}
